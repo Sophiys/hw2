98fa5e232f8cb91bb76234b635e5586a7df800b1
Sophiys <sophiys@mail.ru>
2023-03-19 23:17:32 +0300

    Initial Commit

---
+++ MyHelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyHelloWorld
+//
+//  Created by София Овчинникова on 15.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        groupOfSignals.layer.cornerRadius = 5
+//        groupOfSignals.layer.masksToBounds = true
+        
+        
+    }
+
+
+}
+
